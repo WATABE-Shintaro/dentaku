8a3535f365b72f9879c862d87f32781beb445dda
watabeshintaro <watabe@hapis.k.u-tokyo.ne.jp>
2022-04-28 01:54:52 +0900

    final

---
+++ TEST/Form1.Designer.vb
@@ -0,0 +1,301 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナで必要です。
+    'Windows フォーム デザイナを使用して変更できます。  
+    'コード エディタを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip
+        Me.AToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.AToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem
+        Me.DToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.Button4 = New System.Windows.Forms.Button
+        Me.Button5 = New System.Windows.Forms.Button
+        Me.Button6 = New System.Windows.Forms.Button
+        Me.Button7 = New System.Windows.Forms.Button
+        Me.Button8 = New System.Windows.Forms.Button
+        Me.Button9 = New System.Windows.Forms.Button
+        Me.Button10 = New System.Windows.Forms.Button
+        Me.Button11 = New System.Windows.Forms.Button
+        Me.Button12 = New System.Windows.Forms.Button
+        Me.Button13 = New System.Windows.Forms.Button
+        Me.Button14 = New System.Windows.Forms.Button
+        Me.Button15 = New System.Windows.Forms.Button
+        Me.MTextBox = New System.Windows.Forms.TextBox
+        Me.MPanel = New System.Windows.Forms.Panel
+        Me.Button16 = New System.Windows.Forms.Button
+        Me.MenuStrip1.SuspendLayout()
+        Me.MPanel.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(490, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'AToolStripMenuItem
+        '
+        Me.AToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AToolStripMenuItem2, Me.DToolStripMenuItem1})
+        Me.AToolStripMenuItem.Name = "AToolStripMenuItem"
+        Me.AToolStripMenuItem.Size = New System.Drawing.Size(69, 20)
+        Me.AToolStripMenuItem.Text = "メニュー(&M)"
+        '
+        'AToolStripMenuItem2
+        '
+        Me.AToolStripMenuItem2.Name = "AToolStripMenuItem2"
+        Me.AToolStripMenuItem2.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.AToolStripMenuItem2.Size = New System.Drawing.Size(152, 22)
+        Me.AToolStripMenuItem2.Text = "Start(&S)"
+        '
+        'DToolStripMenuItem1
+        '
+        Me.DToolStripMenuItem1.Name = "DToolStripMenuItem1"
+        Me.DToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.DToolStripMenuItem1.Text = "End(&E)"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(84, 86)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(30, 30)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "&1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(117, 86)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(30, 30)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "&2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(153, 86)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(30, 30)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "&3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(84, 122)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(30, 30)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "&4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(117, 122)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(30, 30)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "&5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(153, 122)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(30, 30)
+        Me.Button6.TabIndex = 7
+        Me.Button6.Text = "&6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(84, 158)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(30, 30)
+        Me.Button7.TabIndex = 8
+        Me.Button7.Text = "&7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(117, 158)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(30, 30)
+        Me.Button8.TabIndex = 9
+        Me.Button8.Text = "&8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(153, 158)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(30, 30)
+        Me.Button9.TabIndex = 10
+        Me.Button9.Text = "&9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(117, 194)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(30, 30)
+        Me.Button10.TabIndex = 11
+        Me.Button10.Text = "&0"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(225, 86)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(30, 30)
+        Me.Button11.TabIndex = 12
+        Me.Button11.Text = "+"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(225, 122)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(30, 30)
+        Me.Button12.TabIndex = 13
+        Me.Button12.Text = "-"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(225, 158)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(30, 30)
+        Me.Button13.TabIndex = 14
+        Me.Button13.Text = "*"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(261, 86)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(30, 30)
+        Me.Button14.TabIndex = 15
+        Me.Button14.Text = "/"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(261, 121)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(30, 66)
+        Me.Button15.TabIndex = 16
+        Me.Button15.Text = "="
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'MTextBox
+        '
+        Me.MTextBox.Location = New System.Drawing.Point(40, 25)
+        Me.MTextBox.Name = "MTextBox"
+        Me.MTextBox.Size = New System.Drawing.Size(350, 19)
+        Me.MTextBox.TabIndex = 17
+        Me.MTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'MPanel
+        '
+        Me.MPanel.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.MPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.MPanel.Controls.Add(Me.Button16)
+        Me.MPanel.Controls.Add(Me.MTextBox)
+        Me.MPanel.Controls.Add(Me.Button14)
+        Me.MPanel.Controls.Add(Me.Button15)
+        Me.MPanel.Controls.Add(Me.Button12)
+        Me.MPanel.Controls.Add(Me.Button1)
+        Me.MPanel.Controls.Add(Me.Button11)
+        Me.MPanel.Controls.Add(Me.Button2)
+        Me.MPanel.Controls.Add(Me.Button13)
+        Me.MPanel.Controls.Add(Me.Button3)
+        Me.MPanel.Controls.Add(Me.Button4)
+        Me.MPanel.Controls.Add(Me.Button5)
+        Me.MPanel.Controls.Add(Me.Button9)
+        Me.MPanel.Controls.Add(Me.Button10)
+        Me.MPanel.Controls.Add(Me.Button6)
+        Me.MPanel.Controls.Add(Me.Button7)
+        Me.MPanel.Controls.Add(Me.Button8)
+        Me.MPanel.Location = New System.Drawing.Point(12, 27)
+        Me.MPanel.Name = "MPanel"
+        Me.MPanel.Size = New System.Drawing.Size(466, 335)
+        Me.MPanel.TabIndex = 18
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(297, 86)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(30, 31)
+        Me.Button16.TabIndex = 18
+        Me.Button16.Text = "C"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(490, 364)
+        Me.Controls.Add(Me.MPanel)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "TEST"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.MPanel.ResumeLayout(False)
+        Me.MPanel.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents AToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AToolStripMenuItem2 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents DToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Button12 As System.Windows.Forms.Button
+    Friend WithEvents Button13 As System.Windows.Forms.Button
+    Friend WithEvents Button14 As System.Windows.Forms.Button
+    Friend WithEvents Button15 As System.Windows.Forms.Button
+    Friend WithEvents MTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents MPanel As System.Windows.Forms.Panel
+    Friend WithEvents Button16 As System.Windows.Forms.Button
+
+End Class
---
+++ TEST/Form1.vb
@@ -0,0 +1,166 @@
+﻿Option Strict On                                     '暗黙の型変更の制限
+
+Public Class Form1
+
+    Private AX As Double
+    Private AY As Double
+    Private ASign As Integer = 0                     '式の段階を表す 0は最初、1はXまで、2は符号まで、3はYまで、4は=まで
+    Private AWhichSign As Integer                    '何の符号か 1は+、2は-、3は*、4は/
+    Private AS2nd As Boolean                         '次の計算に入るときのフラグ
+    Private AMinus As Boolean                        '負のフラグ
+
+    Private Sub First(ByVal TN As Integer)
+        If ASign <= 1 Then
+            If AMinus Then
+                AX = AX * 10 - TN
+                MTextBox.Text = AX.ToString
+                ASign = 1
+            Else
+                AX = AX * 10 + TN
+                MTextBox.Text = AX.ToString
+                ASign = 1
+            End If
+        Else
+            Second(TN)
+        End If
+    End Sub
+    Private Sub SSign(ByVal TSign As Integer)
+        Select Case ASign
+            Case 0
+                If TSign = 2 Then AMinus = True
+            Case 1 To 4
+                If AS2nd Then SToThe2nd()
+                If ASign = 3 Then Main()
+                Select Case TSign
+                    Case 1
+                        AWhichSign = 1
+                    Case 2
+                        AWhichSign = 2
+                    Case 3
+                        AWhichSign = 3
+                    Case 4
+                        AWhichSign = 4
+                End Select
+                ASign = 2
+                AMinus = False
+        End Select
+    End Sub
+
+    Private Sub Second(ByVal TN As Integer)
+        If Not ASign = 3 Then AY = 0
+        AY = AY * 10 + TN
+        MTextBox.Text = AY.ToString
+        If ASign = 4 Then AS2nd = True
+        ASign = 3
+    End Sub
+
+    Private Sub Main()
+        If ASign = 3 Then
+            Select Case AWhichSign
+                Case 1
+                    AX = AX + AY
+                Case 2
+                    AX = AX - AY
+                Case 3
+                    AX = AX * AY
+                Case 4
+                    AX = AX / AY
+            End Select
+            MTextBox.Text = AX.ToString
+        End If
+        AS2nd = False
+    End Sub
+    Private Sub SC()
+        AX = 0
+        AY = 0
+        ASign = 0
+        AWhichSign = 0
+        MTextBox.Text = AX.ToString
+        AS2nd = False
+        AMinus = False
+    End Sub
+    Private Sub SToThe2nd()
+        Dim Temp As Double
+        Temp = AY
+        SC()
+        AX = Temp
+        MTextBox.Text = AX.ToString
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        MPanel.Visible = False
+    End Sub
+
+    Private Sub AToolStripMenuItem2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AToolStripMenuItem2.Click
+        MPanel.Visible = True
+    End Sub
+
+    Private Sub DToolStripMenuItem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DToolStripMenuItem1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        First(1)
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        First(2)
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        First(3)
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        First(4)
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        First(5)
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        First(6)
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        First(7)
+    End Sub
+
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+        First(8)
+    End Sub
+
+    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+        First(9)
+    End Sub
+
+    Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+        First(0)
+    End Sub
+
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        SSign(1)
+    End Sub
+
+    Private Sub Button12_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button12.Click
+        SSign(2)
+    End Sub
+
+    Private Sub Button13_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button13.Click
+        SSign(3)
+    End Sub
+
+    Private Sub Button14_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button14.Click
+        SSign(4)
+    End Sub
+
+    Private Sub Button15_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button15.Click
+        Main()
+        ASign = 4
+    End Sub
+
+    Private Sub Button16_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button16.Click
+        SC()
+    End Sub
+End Class
